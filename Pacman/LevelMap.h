79b64a2d16568f63382737211a4a51f693baf6c1
HarryWheadon <91532361+HarryWheadon@users.noreply.github.com>
2022-08-03 21:34:03 +0100

    updated Levelmap

--- Pacman/LevelMap.h
+++ Pacman/LevelMap.h
@@ -1,5 +1,13 @@
 #pragma once
+#ifndef _LEVELMAP_H
+#define _LEVELMAP_H
+#include "constants.h"
 class LevelMap
 {
+public:
+	LevelMap(int map[MAP_HEIGHT][MAP_WIDTH]);
+	~LevelMap();
+private:
+	int** m_map;
 };
-
+#endif
---
+++ Pacman/constants.h
@@ -0,0 +1,3 @@
+#pragma once
+#define MAP_HEIGHT 13
+#define MAP_WIDTH 32
